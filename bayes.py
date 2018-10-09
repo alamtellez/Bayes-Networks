070fdf4825789f66de65f357bba13b504d0d4167
atellez08 <a01205569@itesm.mx>
2018-10-09 17:49:48 -0500

    Meh

--- bayes.py
+++ bayes.py
@@ -3,8 +3,13 @@ var_nodes = input().rstrip().split(',')
 n_probs = int(input())
 pobabilities = []
 queries = []
+def parse_prob(arr):
+    return arr.rstrip().split("=")
+d_probs = {}
 for i in range(n_probs):
-    parse_prob(input())
+    key, value = parse_prob(input())
+    d_probs["key"] = int(value)
 n_queries = int(input())
 for i in range(n_queries):
     parse_query(input())
+
